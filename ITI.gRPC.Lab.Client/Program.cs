895403d2a55d660b97b1b68127abefcc97da3af9
shadi <shadyayman015@gmail.com>
2024-05-31 23:27:04 +0300

    updated

--- ITI.gRPC.Lab.Client/Program.cs
+++ ITI.gRPC.Lab.Client/Program.cs
@@ -6,35 +6,73 @@ using System.Threading.Tasks;
 var channel = GrpcChannel.ForAddress("https://localhost:7147");
 var client = new Inventory.InventoryClient(channel);
 
-var productId = "123";
-var productRequest = new ProductRequest { ProductId = productId };
+Console.WriteLine("Client: Connected to server.");
+
+// Default product ID to search and update
+var defaultProductId = "123";
+
+var productRequest = new ProductRequest { ProductId = defaultProductId };
+Console.WriteLine($"Client: Sending request to get product with ID: {defaultProductId}");
 var productResponse = await client.GetProductByIdAsync(productRequest);
+Console.WriteLine($"Client: Received response. Product exists: {productResponse.Exists}");
 
 if (productResponse.Exists)
 {
-    // Update product
+    // Product exists, update it
+    Console.WriteLine($"Client: Product with ID {defaultProductId} exists. Please provide updated product details:");
+
+    Console.WriteLine("Enter product name:");
+    var updatedProductName = Console.ReadLine();
+
+    Console.WriteLine("Enter product description:");
+    var updatedProductDescription = Console.ReadLine();
+
+    Console.WriteLine("Enter product price:");
+    var updatedProductPrice = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter product quantity:");
+    var updatedProductQuantity = int.Parse(Console.ReadLine());
+
     var updatedProduct = new Product
     {
-        ProductId = productId,
-        ProductName = "Updated Product Name",
-        ProductDescription = "Updated Product Description",
-        ProductPrice = 99.99,
-        ProductQuantity = 10
+        ProductId = defaultProductId,
+        ProductName = updatedProductName,
+        ProductDescription = updatedProductDescription,
+        ProductPrice = updatedProductPrice,
+        ProductQuantity = updatedProductQuantity
     };
+
+    Console.WriteLine("Client: Sending request to update product.");
     var updateResponse = await client.UpdateProductAsync(updatedProduct);
-    Console.WriteLine($"Product Updated: {updateResponse.Product.ProductName}");
+    Console.WriteLine($"Client: Received response. Product Updated: {updateResponse.Product.ProductName}");
 }
 else
 {
-    // Add new product
+    // Product does not exist, create a new one
+    Console.WriteLine($"Client: Product with ID {defaultProductId} does not exist. Please provide product details to create:");
+
+    Console.WriteLine("Enter product name:");
+    var productName = Console.ReadLine();
+
+    Console.WriteLine("Enter product description:");
+    var productDescription = Console.ReadLine();
+
+    Console.WriteLine("Enter product price:");
+    var productPrice = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter product quantity:");
+    var productQuantity = int.Parse(Console.ReadLine());
+
     var newProduct = new Product
     {
-        ProductId = productId,
-        ProductName = "New Product",
-        ProductDescription = "New Product Description",
-        ProductPrice = 49.99,
-        ProductQuantity = 5
+        ProductId = defaultProductId,
+        ProductName = productName,
+        ProductDescription = productDescription,
+        ProductPrice = productPrice,
+        ProductQuantity = productQuantity
     };
+
+    Console.WriteLine("Client: Sending request to add new product.");
     var addResponse = await client.AddProductAsync(newProduct);
-    Console.WriteLine($"Product Added: {addResponse.Product.ProductName}");
+    Console.WriteLine($"Client: Received response. Product Added: {addResponse.Product.ProductName}");
 }
